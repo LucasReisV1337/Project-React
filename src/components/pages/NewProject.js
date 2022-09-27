e6bdf22f5cba2378c13c1a7ceadc3eb081f01c29
Lucas Reis Vasconcelos <lucasv@kofre.com.br>
2022-09-27 13:07:19 -0300

    final aula #23

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,12 +1,25 @@
+import {useHistory} from 'react-router-dom'
+
 import ProjectForm from '../project/ProjectForm';
 import styles from './NewProject.module.css';
 
 function NewProject() { 
+    
+   //const history = useHistory()
+
+    //function createPost(project) { 
+        
+    // initialize cost and services
+        //project.cost = 0
+        //project.services = []
+    
+   // }
+    
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
             <p>Crie seu projeto para depois adicionar os serviços</p>
-            <ProjectForm/>
+            <ProjectForm btnText="Criar Projeto"/>
         </div>
     )
  }
--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,25 +1,51 @@
+import { useEffect } from 'react'
+import { useState } from 'react'
+
+
+import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
-function ProjectForm() { 
+function ProjectForm({ btnText }) { 
+    
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => { 
+        fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            'Content-Type':'application/json'
+        }
+    }).then((resp) => resp.json())
+        .then((data) => { 
+            setCategories(data)
+        })
+        .catch(err => console.log(err))
+    },[])
+
     return (
         <form className={styles.form}>
+            <Input
+                type="text"
+                text="Nome do projeto"
+                name="name"
+                placeholder="Insira o nome do projeto"
+            />
+            <Input
+                type="number"
+                text="Orçamento do projeto"
+                name="budget"
+                placeholder="Insira o orçamento do projeto"
+            />
             <div>
-            <input type="text" placeholder="Insira o nome do projeto" />
+                <Select
+                    name="category_id"
+                    text="Selecione a categoria"
+                    options={categories}
+                />
             </div>
-            <div>
-            <input type="number" placeholder="Insira o orçamento total" />
-            </div>
-            <div>
-            <select name="category_id">
-                    <option disabled>
-                        Selecione a categoria
-                    </option>
-            </select>
-            </div>
-            <div>
-            <input type="submit" value="Criar projeto" />
-
-            </div>
+            <SubmitButton text={btnText} />
 
        </form>
     )
